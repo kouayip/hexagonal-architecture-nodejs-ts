056299af2a77f49b7eea3f5880ca32ab95f58953
Yves Kams <kamwayves37@gmail.com>
2024-02-11 23:32:21 +0100

    refactor: load module in inversify config

--- src/infrastructure/fastify/server.ts
+++ src/infrastructure/fastify/server.ts
@@ -2,15 +2,12 @@ import Fastify, { FastifyInstance } from "fastify";
 import { Container } from "inversify";
 import UserController from "~/interfaces/http/user.controller";
 import { FastifyControllerBuilder } from "~/core/infrastructure/fastify";
-import { userModule } from "~/interfaces/http/user.module";
 
 export default class FastifyServer {
     static build(container: Container): FastifyInstance {
         const fastify = Fastify({
             logger: true,
         });
-
-        container.load(userModule);
 
         const builder = new FastifyControllerBuilder(fastify, container);
 
--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -1,5 +1,8 @@
 import { Container } from "inversify";
+import { userModule } from "~/interfaces/http/user.module";
 
 const container = new Container();
+
+container.load(userModule);
 
 export default container;
